20f2172a319bf354419b220b46650e87fb8192ef
sysu-server <2716705056@qq.com>
2025-02-17 11:45:49 +0800

    just update

--- 通信仿真/JIAFEIMAO的摆烂日常/AMPTutorial-main/TutorialAMP_Github_V2/EXP1/test.m
+++ 通信仿真/JIAFEIMAO的摆烂日常/AMPTutorial-main/TutorialAMP_Github_V2/EXP1/test.m
@@ -1,77 +1,19 @@
 clc;clear all;close all;
 
 
-%% Parameters
-N=1024;
-M=512;
-rho=0.05;
-snr_dB=50;
-TestNum=1e1;
-IterNum=3*1e2;
-lambda=0.05;
 
-
-%% Load parameters
-Input.N=N;
-Input.M=M;
-Input.rho=rho;
-Input.nuw=10^(-snr_dB/10);
-Input.IterNum=IterNum;
-Input.lambda=lambda;
-
-obj=system_model(Input);
-%% Load parameters
-IterNum=Input.IterNum;
-M=Input.M;
-N=Input.N;
-lambda=Input.lambda;
-y=obj.y;
-H=obj.H;
-hatx=zeros(N,1);
-delta=M/N;
-gamma=1;
-Onsager=0;
-mes=0.95;
-z_old=0;
-
-for ii=1:IterNum
-    z=y-H*hatx+Onsager;
-    [z, z_old]=damping(z, z_old ,mes);
-    r=hatx+H'*z;
-    hatx=sign(r).*max(abs(r)-lambda-gamma,0);
-    tem=mean_partial(hatx+H'*z, lambda+gamma);
-    Onsager=1/delta*z*mean_partial(hatx+H'*z, lambda+gamma);   
-    gamma=(lambda+gamma)/delta*mean_partial(hatx+H'*z, lambda+gamma);
-    
-    MSE(ii,1)=norm(obj.x-hatx)^2/norm(obj.x)^2;
-    error(ii,1)=norm(y-H*hatx);
-end
-
-figure (2);
-plot(obj.x, 'r');
-hold on;
-figure (3);
-plot(hatx, 'b');
-hold on;
-legend
-
-function mean_out = mean_partial(R, Sigma)
-    N=length(R);
-    tem=zeros(N,1);
-    for ii=1:N
-        if abs(R(ii))>Sigma
-            tem(ii)=1;
-        end
-    end
-    mean_out=mean(tem);
-end
-
-function [x, x_old]=damping(x, x_old, mes)
-    x=mes*x+(1-mes)*x_old;
-    x_old=x;
-end
-
-
-
-
-
+snrdB=-10:0.5:30; %Range of SNRs to simulate
+h= (randn(1,100) + 1i*randn(1,100) )/sqrt(2); %Rayleigh flat channel 
+sigma_z=1; %Noise power - assumed to be unity
+snr = 10.^(snrdB/10); %SNRs in linear scale 
+P=(sigma_z^2)*snr./(mean(abs(h).^2)); %corresponding values for P
+C_awgn=(log2(1+ mean(abs(h).^2).*P/(sigma_z^2)));%C_{awgn} 
+C_fading=mean((log2(1+ ((abs(h).^2).')*P/(sigma_z^2))));%C_{fading}
+plot(snrdB,C_awgn,'b'); 
+hold on; 
+plot(snrdB,C_fading,'r'); 
+grid on; 
+legend('AWGN channel capacity','Fading channel Ergodic capacity'); 
+title('SISO fading channel - Ergodic capacity'); 
+xlabel('SNR (dB)');
+ylabel('Capacity (bps/Hz)');
--- 通信仿真/无线通信系统/ChannelCapacity.py
+++ 通信仿真/无线通信系统/ChannelCapacity.py
@@ -4,14 +4,16 @@
 Created on Mon Feb 17 00:22:31 2025
 
 @author: jack
-"""
 
+<Wireless communication systems in Matlab> Chap3
+"""
+#%%
 import scipy
 import numpy as np
 import statsmodels.tsa.api as smt
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
-
+import commpy
 
 # 全局设置字体大小
 plt.rcParams["font.family"] = "Times New Roman"
@@ -33,9 +35,7 @@ plt.rcParams['figure.facecolor'] = 'white'  # 设置图形背景色为浅灰色
 plt.rcParams['axes.edgecolor'] = 'black'  # 设置坐标轴边框颜色为黑色
 plt.rcParams['legend.fontsize'] = 20
 
-
-
-#%% Program 3.1: Shannon limit.m: Dependency of spectral and power efﬁciencies for AWGN channel
+#%% Program 3.1: Shannon limit.m: Dependency of spectral and power efficiencies for AWGN channel
 k = np.arange(0.1, 15, 0.001)
 EbN0 = (2**k-1)/k
 EbN0dB = 10 * np.log10(EbN0)
@@ -48,30 +48,41 @@ axs.set_xlabel(r'$\mathrm{E_b}/\mathrm{N_0}$(dB)',)
 axs.set_ylabel('Spectral Efficiency (Bit/s/Hz)',)
 axs.set_title("Channel Capacity & Power efficiency limit")
 
-axs.vlines(x = -1.59, ymin = 0.08 ,ymax = 13, color = 'red', ls = '--')
-
+axs.vlines(x = -1.59, ymin = 0.08 , ymax = 14, color = 'red', ls = '--')
 axs.text( 4, 8, "R > C\nUnattainable", color='red', size = 20, rotation = 0., ha = "center", va="center",)
 axs.text(16, 0.3, "R < C\nPractical systems", color='g', size = 20, rotation = 0., ha = "center", va="center",)
 axs.text( 16, 6, "R = C\ncapacity boundary", color='k', size = 20, rotation = 16., ha = "center", va="center",)
-# axs.text( 1.1, 0.04, "Shannon limit\n-1.59dB", color='k', size = 20, rotation = 0., ha = "center",  va = "center", wrap = True)
-axs.annotate("Shannon limit\n-1.59dB", xy = (0.155, 0.2) , xytext = (0.06,0.01) , size = 20, xycoords = 'figure fraction', textcoords='figure fraction', arrowprops=dict(facecolor='fuchsia',arrowstyle='<|-',connectionstyle='arc3'))
+axs.annotate("Shannon limit\n-1.59dB", xy = (0.155, 0.2) , xytext = (0.06,0.01) , size = 20, xycoords = 'figure fraction', textcoords='figure fraction', arrowprops=dict(color='r',arrowstyle='<|-',connectionstyle='arc3'))
 # axs.legend()
 plt.show()
 plt.close()
 
-
-
-
 #%% Program 3.7: ergodic capacity limits.m: Simulating the ergodic capacity of a fading channel
 snrdB = np.arange(-10, 30, 1/2)
-h = (np.random.randn( 10000) + 1j * np.random.randn( 10000))/np.sqrt(2)
+h = (np.random.randn(1, 10000) + 1j * np.random.randn(1, 10000))/np.sqrt(2)
 sigma_z = 1
 snr = 10**(snrdB/10)
 P = (sigma_z**2) * snr / np.mean(np.abs(h)**2)
 
-C_agwn = 1
+C_awgn = np.log2(1 + np.mean(np.abs(h)**2) * P / (sigma_z**2))
+C_fading = np.mean(np.log2(1 + (np.abs(h)**2).T @ P.reshape(1, -1) / sigma_z**2 ), axis = 0)
 
+##### plot
+fig, axs = plt.subplots(1, 1, figsize = (8, 6), constrained_layout = True)
 
+axs.plot(snrdB, C_awgn, color = 'b', ls = '--',  )
+axs.plot(snrdB, C_fading, color = 'r', ls = '-',  )
+axs.text(18, 6.5, "AWGN channel capacity", color='k', size = 20, rotation = 44., ha = "center", va="center",)
+axs.text(20, 5.4, "Fading channel Ergodic capacity", color='k', size = 20, rotation = 43., ha = "center", va="center",)
+
+axs.set_xlabel( 'SNR(dB)',)
+axs.set_ylabel('Capacity (Bit/s/Hz)',)
+axs.set_title("SISO fading channel - Ergodic capacity")
+
+plt.show()
+plt.close()
+
+#%% Program 3.6: CapacityDCMC.m: Capacity of M-ary transmission through DCMC AWGN channel
 
 
 
