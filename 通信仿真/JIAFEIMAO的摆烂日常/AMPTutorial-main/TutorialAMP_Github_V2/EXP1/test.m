bf45c1bcb052318840168a3e363609f98a98d67d
sysu-server <2716705056@qq.com>
2025-02-17 15:20:07 +0800

    just update

--- 通信仿真/JIAFEIMAO的摆烂日常/AMPTutorial-main/TutorialAMP_Github_V2/EXP1/test.m
+++ 通信仿真/JIAFEIMAO的摆烂日常/AMPTutorial-main/TutorialAMP_Github_V2/EXP1/test.m
@@ -2,18 +2,37 @@ clc;clear all;close all;
 
 
 
-snrdB=-10:0.5:30; %Range of SNRs to simulate
-h= (randn(1,100) + 1i*randn(1,100) )/sqrt(2); %Rayleigh flat channel 
-sigma_z=1; %Noise power - assumed to be unity
-snr = 10.^(snrdB/10); %SNRs in linear scale 
-P=(sigma_z^2)*snr./(mean(abs(h).^2)); %corresponding values for P
-C_awgn=(log2(1+ mean(abs(h).^2).*P/(sigma_z^2)));%C_{awgn} 
-C_fading=mean((log2(1+ ((abs(h).^2).')*P/(sigma_z^2))));%C_{fading}
-plot(snrdB,C_awgn,'b'); 
-hold on; 
-plot(snrdB,C_fading,'r'); 
-grid on; 
-legend('AWGN channel capacity','Fading channel Ergodic capacity'); 
-title('SISO fading channel - Ergodic capacity'); 
-xlabel('SNR (dB)');
-ylabel('Capacity (bps/Hz)');
+nBits=10^5; %number of bits to transmit 
+errorProbs = 0:0.1:0.6; %cross-over probabilities of BSC to simulate 
+C = zeros(1,length(errorProbs)); %to store capacity for each point
+ 
+j=1; 
+for e=errorProbs, %for each error probability 
+    x = rand(1,nBits)<0.5; %uniform source bits of 1's and 0's 
+    y = bsc_channel(x,e); %process the bits through BSC
+ 
+    Y = repmat(y,2,1);%construct 2xL matrix from y 
+    X = repmat([0;1],1,nBits);%construct 2xL matrix from [0;1] %probabilities with respect to each input[0,1] 
+
+    prob = (Y==X)*(1-e)+(Y~=X)*e; %BSC p(y/x) equation 
+    prob = max(prob,realmin);%this used to avoid NAN in computation 
+    p = prob./(ones(2,1)*(sum(prob)));%normalize probabilities 
+    HYX = mean(-sum(p.*log2(p))); %senders uncertainity
+
+    py0 = sum(y==0)/nBits; %py(y=0) 
+    py1 =sum(y==1)/nBits; %py(y=1) 
+    HY = -py0*log2(py0) - py1*log2(py1);%avg info content of output 
+    C(j) = HY-HYX; %capacity 
+    j=j+1;
+
+end 
+plot(errorProbs,C); title('Capacity over Binary Symmetric Channel');
+xlabel('Cross-over probability - e'); 
+ylabel('Capacity (bits/channel use)');
+
+
+function y = bsc_channel(x,e) %Process an input vector x through a Binary Symmetric Channel with %error probability e. 
+y = x; %copy the input vector 
+errors = rand(1,length(x))<e; %Bernoulli trials with probability e 
+y(errors) = 1 - y(errors); %flip the bits
+end
