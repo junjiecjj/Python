e3a059a441a18b3a59a3aa7456b5dde18d22bbb4
sysu-server <2716705056@qq.com>
2025-02-18 18:27:04 +0800

    just update

--- FL_1bitJoint/SISO_NOMA_JointSIC/NOMA_capactityOptim.py
+++ FL_1bitJoint/SISO_NOMA_JointSIC/NOMA_capactityOptim.py
@@ -17,6 +17,15 @@ from Channel import AWGN_mac, BlockFading_mac, FastFading_mac, Large_mac
 
 
 
+B = 4e6 # Hz
+sigma2 = -60 # dBm/Hz
+sigma2 = 10**(sigma2/10.0)/1000 # w/Hz
+N0 = sigma2 * B # w
+
+pmax = 30 # dBm
+pmax =10**(pmax/10.0)/1000 # Watts
+pmax = 0.1      # Watts
+
 
 K = 100
 BS_locate, users_locate, beta_Au, PL_Au = channelConfig(K, r = 100)
