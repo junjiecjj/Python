83f6fb3abc2495f833c5c0af64ddf4a6221ffbe2
ustc-desktop <2716705056@qq.com>
2025-02-20 02:36:51 +0800

    just update

--- 通信仿真/无线通信系统/Chap10_LargescalePropagationModels.py
+++ 通信仿真/无线通信系统/Chap10_LargescalePropagationModels.py
@@ -179,28 +179,97 @@ axs.set_ylabel('Normalized Received power (in dB)',)
 axs.set_title("Two ray ground reflection model", fontsize = 18)
 axs.legend()
 
-# my_x_ticks = [1,2,4,8,16,32]
-# my_y_ticks = np.arange(-40, -95, -5)
-# axs.set_xticks(my_x_ticks)
-# axs.set_yticks(my_y_ticks)
-# plt.xticks([1,2,4,8,16,32,35])
-
 plt.show()
 plt.close()
 
 
 
+#%% Program 10.6: diffractionLoss.m : Function to calculate diffraction loss/gain
+
+def diffractionLoss(v):
+    # Compute diffraction loss G(v)dB for Fresnel-Kirchoff parameter(v)
+    # according to Rec. ITU-R P.526-5
+    Gv = np.zeros(v.size)
+    idx = np.where(v > -0.7)[0] # indices where v>0.7
+
+    Gv[idx] = 6.9 + 20 * np.log10(np.sqrt((v[idx]-0.1)**2 + 1) + v[idx] -0.1) # dB
+    Gv[np.where(Gv <= -0.7) ] = 0          #  v<-0.7
+    return Gv
+
+v = np.arange(-5, 21, 1, )
+Ld = diffractionLoss(v)
 
 
+##### plot
+fig, axs = plt.subplots(1, 1, figsize = (8, 6), constrained_layout = True)
+
+# x
+axs.plot(v, -Ld, color = 'b',  label = ' ', )
+
+axs.set_xlabel("Fresnel-Kirchoff parameter (v)")
+axs.set_ylabel('Diffraction gain - G_d(v) dB',)
+axs.set_title("Diffraction Gain Vs. Fresnel-Kirchoff parameter", fontsize = 18)
+# axs.legend()
+
+plt.show()
+plt.close()
+
+#%% Program 10.12: Hata model.m: Function to simulate Hata-Okumura model
+def Hata_model(fc,d,hb,hm,envType):
+    #Hata model for propagation loss
+    #   fc - frequency in MHz (single value)
+    #   d - array of distances (in Km) to simulate
+    #   hb - effective base station height in meters
+    #   hm - effective mobile height in meters
+    #   envType - 'metro','smallcity','suburban' or 'open'
+    #   returns the path loss (PL) in dB
+    # Warning: Hata model is valid only if the model parameters
+    # falls within certain range of values(see text). This function has not implemented those range checks.
+    envType = envType.lower()
+    if envType == "metro":
+        C = 0
+        if fc <= 200:
+            aHm = 8.29 *(np.log10(1.54*hm))**2 - 1.1
+        else:
+            aHm = 3.2*(np.log10(11.75*hm))**2 - 4.97
+    elif envType == 'smallcity':
+        C = 0
+        aHm = (1.1 * np.log10(fc)-0.7)*hm - (1.56 * np.log10(fc) - 0.8)
+    elif envType == 'suburban':
+        aHm = (1.1 * np.log10(fc)-0.7)*hm - (1.56 * np.log10(fc) - 0.8)
+        C=-2*(np.log10(fc/28))**2 - 5.4
+    elif envType == 'open':
+        aHm = (1.1 * np.log10(fc) - 0.7)*hm - (1.56*np.log10(fc)-0.8)
+        C = -4.78*(np.log10(fc))**2 + 18.33 * np.log10(fc) - 40.98
+    A = 69.55 + 26.16 * np.log10(fc) - 13.82 * np.log10(hb) - aHm
+    B = 44.9 - 6.55*np.log10(hb)
+    PL = A + B * np.log10(d)+C
+    return PL
 
 
+hb=70
+hm=1.5    # effective BS and MS heights in meters
+fc = 1500 # Frequency given in MHz
+d = np.arange(1, 100, 1) # 1:1:100  # distances in Kms
 
+PL1 = Hata_model(fc,d,hb,hm,'suburban') #semilogx(d,PL,'b'); hold on;
+PL2 = Hata_model(fc,d,hb,hm,'open')  # semilogx(d,PL,'r');
+PL3 = Hata_model(fc,d,hb,hm,'metro') #semilogx(d,PL,'k');
 
+##### plot
+fig, axs = plt.subplots(1, 1, figsize = (8, 6), constrained_layout = True)
 
+# x
+axs.semilogx(d, PL1, color = 'b',  label = 'Suburban area', )
+axs.semilogx(d, PL2, color = 'r',  label = 'Open area', )
+axs.semilogx(d, PL3, color = 'k',  label = 'Metropolitan area', )
+axs.set_xlabel("Distance (km)")
+axs.set_ylabel('Propagation path Loss (dB)',)
+axs.set_title("Hata-Okumura Path Loss Model", fontsize = 18)
+axs.legend()
 
-
-
-
+plt.show()
+plt.close()
 
 
 
