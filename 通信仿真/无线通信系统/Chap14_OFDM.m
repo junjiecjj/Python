0d9a05c48e729efee9e17e3081388b236075ed40
ustc-desktop <2716705056@qq.com>
2025-02-21 02:00:38 +0800

    just update

--- 通信仿真/无线通信系统/Chap14_OFDM.m
+++ 通信仿真/无线通信系统/Chap14_OFDM.m
@@ -1,12 +1,9 @@
- 
+
 % https://www.cnblogs.com/gjblog/p/13352746.html
 
 % https://www.cnblogs.com/gjblog/p/13363995.html
 
 % https://github.com/loveybos/CommunicationSys
-
-
-
 
 
 %%  https://www.cnblogs.com/gjblog/p/13352746.html
@@ -28,14 +25,14 @@
 % NCP = 16; % 循环前缀长度
 % NSYM = NFFT + NCP; % OFDM符号长度
 % M = 16; K = 4; % M:调制阶数，K:log2(M)
-% 
+%
 % EbN0 = 0:1:20; % 设出比特信噪比(dB)
 % snr = EbN0 + 10 * log10(K); % 由他俩关系推出(dB)
 % BER(1 : length(EbN0)) = 0; % 初始化误码率
-% 
+%
 % file_name=['OFDM_BER_NCP' num2str(NCP) '.dat'];
 % fid=fopen(file_name, 'w+');
-% 
+%
 % X = randi([0,15],1,NFFT * NFRAME); % 生成数字基带信号
 % %%
 % for i = 1 : length(EbN0) % 对于每一种比特信噪比，计算该通信环境下的误码率
@@ -44,16 +41,16 @@
 %     %% IFFT与循环前缀添加
 %     x(1 : NFFT * NFRAME) = 0; % 预分配x数组
 %     xt(1 : (NFFT + NCP) * NFRAME) = 0; % 预分配x_t数组
-% 
+%
 %     len_a = 1 : NFFT; % 处理的X位置
 %     len_b = 1 : (NFFT + NCP); % 处理的X位置(加上CP的长度)
 %     len_c = 1 : NCP;
 %     len_left = 1 : (NFFT / 2); len_right = (NFFT / 2 + 1) : NFFT; % 每一符号分为左右两边？？
-% 
+%
 %     for frame = 1 : NFRAME % 对于每个OFDM符号都要翻转和IFFT
 %         x(len_a) = ifft([X_mod(len_right), X_mod(len_left)]); % 左右翻转再ifft
 %         xt(len_b) = [x(len_c + NFFT - NCP), x(len_a)]; % 添加CP后的信号数组
-% 
+%
 %         len_a = len_a + NFFT; % 更新找到下一个符号起点位置
 %         len_b = len_b + NFFT + NCP; % 更新找到下一个符号起点位置(CP开头)
 %         len_c = len_c + NFFT;
@@ -67,17 +64,17 @@
 %     %% 去除循环前缀并且FFT
 %     y(1 : NFFT * NFRAME) = 0; % 预分配y数组
 %     Y(1 : NFFT * NFRAME) = 0; % 预分配Y数组
-% 
+%
 %     len_a = 1 : NFFT; % 处理的y位置
 %     len_b = 1 : NFFT; % 处理的y位置
 %     len_left = 1 : (NFFT / 2); len_right = (NFFT / 2 + 1) : NFFT; % 每一符号分为左右两边
-% 
+%
 %     for frame = 1 : NFRAME % 对于每个OFDM符号先去CP，再FFT再翻转
 %         y(len_a) = yr(len_b + NCP); % 去掉CP
-% 
+%
 %         Y(len_a) = fft(y(len_a)); % 先fft再翻转
 %         Y(len_a) = [Y(len_right), Y(len_left)];
-% 
+%
 %         len_a = len_a + NFFT;
 %         len_b = len_b + NFFT + NCP;
 %         len_left = len_left + NFFT; len_right = len_right + NFFT;
@@ -88,7 +85,7 @@
 %     Neb = sum(sum(de2bi(Yr,K) ~= de2bi(X,K))); % 转为2进制，计算具体有几bit错误
 %     Ntb = NFFT * NFRAME * K;  %仿真的总比特数
 %     BER(i) = Neb / Ntb;
-% 
+%
 %     fprintf('EbN0 = %d[dB], BER = %d / %d = %.3f\n', EbN0(i),Neb,Ntb,BER(i))
 %     fprintf(fid, '%d %5.3f\n', EbN0(i),BER(i));
 % end
---
+++ 通信仿真/无线通信系统/Chap8_LinearEqualizers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 01:16:48 2025
+
+@author: jack
+"""
+
