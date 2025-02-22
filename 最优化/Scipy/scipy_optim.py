15c05780064a223090bb81cdc2b2de5224beabe3
sysu-server <2716705056@qq.com>
2025-02-22 19:56:10 +0800

    just update

--- 最优化/Scipy/scipy_optim.py
+++ 最优化/Scipy/scipy_optim.py
@@ -706,9 +706,9 @@ xgrid, ygrid = np.meshgrid(x, y)
 xy = np.stack([xgrid, ygrid])
 
 fig = plt.figure(figsize = (8, 6), constrained_layout = True)
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection = '3d')
 ax.view_init(45, -45)
-ax.plot_surface(xgrid, ygrid, eggholder(xy), cmap='terrain')
+ax.plot_surface(xgrid, ygrid, eggholder(xy), cmap = 'terrain')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('eggholder(x, y)')
@@ -719,15 +719,15 @@ results = {}
 results['shgo'] = optimize.shgo(eggholder, bounds)
 print(results['shgo'])
 
-results = {}
+# results = {}
 results['DA'] = optimize.dual_annealing(eggholder, bounds)
 print(results['DA'])
 
-results = {}
+# results = {}
 results['DE'] = optimize.differential_evolution(eggholder, bounds)
 print(results['DE'])
 
-results = {}
+# results = {}
 results['shgo_sobol'] = optimize.shgo(eggholder, bounds, n=200, iters=5, sampling_method='sobol')
 print(results['shgo_sobol'])
 
@@ -754,7 +754,6 @@ ax.set_xlim([-4, 514*2])
 ax.set_ylim([-4, 514*2])
 plt.show()
 
-
 ##====================================================================================
 #  Least-squares minimization (least_squares)
 ##====================================================================================
@@ -777,13 +776,10 @@ def jac(x, u, y):
     J[:, 3] = -x[0] * num / den ** 2
     return J
 
-
 u = np.array([4.0, 2.0, 1.0, 5.0e-1, 2.5e-1, 1.67e-1, 1.25e-1, 1.0e-1, 8.33e-2, 7.14e-2, 6.25e-2])
 y = np.array([1.957e-1, 1.947e-1, 1.735e-1, 1.6e-1, 8.44e-2, 6.27e-2, 4.56e-2, 3.42e-2, 3.23e-2, 2.35e-2, 2.46e-2])
 x0 = np.array([2.5, 3.9, 4.15, 3.9])
 res = least_squares(fun, x0, jac = jac, bounds=(0, 100), args=(u, y), verbose=1)
-
-
 print(res.x)
 
 import matplotlib.pyplot as plt
