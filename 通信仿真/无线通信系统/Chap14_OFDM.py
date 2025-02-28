7d47030a114ab0a12ba81b64d51bc30c1b3bd573
ustc-desktop <2716705056@qq.com>
2025-03-01 01:52:20 +0800

    just update

--- 通信仿真/无线通信系统/Chap14_OFDM.py
+++ 通信仿真/无线通信系统/Chap14_OFDM.py
@@ -36,36 +36,75 @@ plt.rcParams['legend.fontsize'] = 22
 
 
 #%%
+# 生成 循环矩阵
+def CirculantMatric(gen, row):
+     if type(gen) == list:
+          col = len(gen)
+     elif type(gen) == np.ndarray:
+          col = gen.size
+     row = col
+
+     mat = np.zeros((row, col), dtype = gen.dtype)
+     mat[:, 0] = gen
+     for i in range(1, row):
+          mat[:,i] = np.roll(gen, i)
+     return mat
+
+def circularConvolve(h, s, N):
+    if h.size < N:
+        h = np.hstack((h, np.zeros(N-h.size)))
+    col = N
+    row = s.size
+    H = np.zeros((row, col), dtype = s.dtype)
+    H[:, 0] = h
+    for i in range(1, row):
+          H[:,i] = np.roll(h, i)
+    res = H @ s
+    return res
+
+# # generateVec =  [1+1j, 2+2j, 3+3j, 4+1j ]
+# generateVec =  [1 , 2  , 3 , 4  ]
+
+# X = np.array(generateVec)
+
+# L = len(generateVec)
+# A = CirculantMatric(X, L)
+
+N = 8
+h = np.array([-0.4878, -1.5351, 0.2355])
+s = np.array([-0.0155, 2.5770, 1.9238, -0.0629, -0.8105, 0.6727, -1.5924, -0.8007])
+
+lin_s_h = scipy.signal.convolve(h, s)
+cir_s_h = circularConvolve(h, s, N)
+
+Ncp = 2
+s_cp = np.hstack((s[-Ncp:], s))
+lin_scp = scipy.signal.convolve(h, s_cp)
+r = lin_scp[Ncp:Ncp+N]
+print(f"lin_scp = \n{lin_scp[Ncp:Ncp+N]}\ncir_s_h = \n{cir_s_h}")
 
 
+R = scipy.fft.fft(r, N)
+H = scipy.fft.fft(h, N)
+S = scipy.fft.fft(s, N)
+
+r1 = scipy.fft.ifft(S*H)
+
+print(f"r1 = \n{r1}\ncir_s_h = \n{cir_s_h}")
 
 
+#%% Program 14.2: add cyclic preﬁx.m: Function to add cyclic preﬁx of length Ncp symbols
+
+def add_cyclic_prefix(x, Ncp):
+    s = np.hstack((x[-Ncp:], x))
+    return s
 
 
+#%% Program 14.3: remove cyclic preﬁx.m: Function to remove the cyclic preﬁx from the OFDM symbol
 
-
-
-
-
-
-
-
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-#%%
-
-
+def remove_cyclic_prefix(r,Ncp,N):
+    y = r[Ncp : Ncp+N]
+    return y
 
 
 
