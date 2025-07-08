e0ed78c5a0d74d463a7ef70dc0acd99047761a29
sysu-server <2716705056@qq.com>
2025-07-08 22:48:55 +0800

    just update

---
+++ 稀疏贝叶斯学习/稀疏贝叶斯学习.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 22:46:24 2025
+
+@author: jack
+
+https://zhuanlan.zhihu.com/p/1893622601374468027
+
+https://blog.csdn.net/qq_44648285/article/details/143313531
+
+https://blog.csdn.net/weixin_40735720/article/details/148583124
+
+https://blog.csdn.net/qq_45471796/article/details/130487580
+
+https://github.com/al5250/sparse-bayes-learn
+"""
+
