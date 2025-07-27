e6265a7a42537ab3eed89b8eeac1e1732ea56107
sysu-server <2716705056@qq.com>
2025-07-27 22:52:07 +0800

    just update

--- 稀疏贝叶斯学习/高斯过程.py
+++ 稀疏贝叶斯学习/高斯过程.py
@@ -4,6 +4,7 @@
 
 
 #%% https://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpc_iris.html#sphx-glr-auto-examples-gaussian-process-plot-gpc-iris-py
+##
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -195,7 +196,6 @@ class ExactGPModel(gpytorch.models.ExactGP):
         self.mean_module = gpytorch.means.ConstantMean()
         # 协方差函数：采用 RBF 核，并用 ScaleKernel 包装
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
@@ -235,9 +235,9 @@ likelihood.eval()
 # 预测：在测试数据上计算后验分布
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
     test_pred = likelihood(model(test_x))
-    pred_mean = test_pred.mean
-    pred_var = test_pred.variance
-    lower, upper = test_pred.confidence_region()
+    pred_mean = test_pred.mean # 500
+    pred_var = test_pred.variance # 500
+    lower, upper = test_pred.confidence_region() # 500, 500
 
 # 绘制图形：4个子图放在一幅图中
 fig, axes = plt.subplots(2, 2, figsize=(15, 10), constrained_layout = True)
@@ -279,12 +279,6 @@ axes[1, 1].legend()
 axes[1, 1].grid(True, linestyle='--', alpha=0.6)
 
 plt.show()
-
-
-
-
-
-
 
 #%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 8. 高斯过程回归插值
 # 十大数据差值方法
@@ -344,64 +338,281 @@ plt.show()
 
 
 
+#%% Gaussian Processes regression: basic introductory example
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
 
 
+X = np.linspace(start=0, stop=10, num=1000).reshape(-1, 1)
+y = np.squeeze(X * np.sin(X))
+fig, ax = plt.subplots( figsize=(8, 6), constrained_layout = True)
+ax.plot(X, y, label=r"$f(x) = x \sin(x)$", linestyle="dotted")
+ax.legend()
+ax.set_xlabel("$x$")
+ax.set_ylabel("$f(x)$")
+ax.set_title("True generative process")
+plt.show()
+plt.close()
+
+rng = np.random.RandomState(1)
+training_indices = rng.choice(np.arange(y.size), size=6, replace=False)
+X_train, y_train = X[training_indices], y[training_indices]
+
+kernel = 1 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
+gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+gaussian_process.fit(X_train, y_train)
+
+mean_prediction, std_prediction = gaussian_process.predict(X, return_std=True)
+fig, ax = plt.subplots( figsize=(8, 6), constrained_layout = True)
+ax.plot(X, y, label=r"$f(x) = x \sin(x)$", linestyle="dotted")
+ax.scatter(X_train, y_train, label="Observations")
+ax.plot(X, mean_prediction, label="Mean prediction")
+ax.fill_between(
+    X.ravel(),
+    mean_prediction - 1.96 * std_prediction,
+    mean_prediction + 1.96 * std_prediction,
+    alpha=0.5,
+    label=r"95% confidence interval",
+)
+ax.legend()
+ax.set_xlabel("$x$")
+ax.set_ylabel("$f(x)$")
+ax.set_title("Gaussian process regression on noise-free dataset")
+plt.show()
+plt.close()
 
 
+# Example with noisy targets
+noise_std = 0.75
+y_train_noisy = y_train + rng.normal(loc=0.0, scale=noise_std, size=y_train.shape)
 
 
+gaussian_process = GaussianProcessRegressor(kernel=kernel, alpha=noise_std**2, n_restarts_optimizer=9)
+gaussian_process.fit(X_train, y_train_noisy)
+mean_prediction, std_prediction = gaussian_process.predict(X, return_std=True)
+
+fig, ax = plt.subplots( figsize=(8, 6), constrained_layout = True)
+ax.plot(X, y, label=r"$f(x) = x \sin(x)$", linestyle="dotted")
+ax.errorbar( X_train, y_train_noisy, noise_std, linestyle="None", color="tab:blue", marker=".", markersize=10, label="Observations", )
+ax.plot(X, mean_prediction, label="Mean prediction")
+ax.fill_between( X.ravel(), mean_prediction - 1.96 * std_prediction, mean_prediction + 1.96 * std_prediction, color="tab:orange", alpha=0.5, label=r"95% confidence interval",)
+ax.legend()
+ax.set_xlabel("$x$")
+ax.set_ylabel("$f(x)$")
+ax.set_title("Gaussian process regression on a noisy dataset")
+plt.show()
+plt.close()
 
 
+#%% Illustration of prior and posterior Gaussian process for different kernels
 
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+def plot_gpr_samples(gpr_model, n_samples, ax):
+    """Plot samples drawn from the Gaussian process model.
+
+    If the Gaussian process model is not trained then the drawn samples are
+    drawn from the prior distribution. Otherwise, the samples are drawn from
+    the posterior distribution. Be aware that a sample here corresponds to a
+    function.
+
+    Parameters
+    ----------
+    gpr_model : `GaussianProcessRegressor`
+        A :class:`~sklearn.gaussian_process.GaussianProcessRegressor` model.
+    n_samples : int
+        The number of samples to draw from the Gaussian process distribution.
+    ax : matplotlib axis
+        The matplotlib axis where to plot the samples.
+    """
+    x = np.linspace(0, 5, 100)
+    X = x.reshape(-1, 1)
+
+    y_mean, y_std = gpr_model.predict(X, return_std=True)
+    y_samples = gpr_model.sample_y(X, n_samples)
+
+    for idx, single_prior in enumerate(y_samples.T):
+        ax.plot(
+            x,
+            single_prior,
+            linestyle="--",
+            alpha=0.7,
+            label=f"Sampled function #{idx + 1}",
+        )
+    ax.plot(x, y_mean, color="black", label="Mean")
+    ax.fill_between(
+        x,
+        y_mean - y_std,
+        y_mean + y_std,
+        alpha=0.1,
+        color="black",
+        label=r"$\pm$ 1 std. dev.",
+    )
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_ylim([-3, 3])
+
+# Dataset and Gaussian process generation
+rng = np.random.RandomState(4)
+X_train = rng.uniform(0, 5, 10).reshape(-1, 1)
+y_train = np.sin((X_train[:, 0] - 2.5) ** 2)
+n_samples = 5
+
+############# Radial Basis Function kernel
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+kernel = 1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0))
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 12))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Radial Basis Function kernel", fontsize=18)
+plt.tight_layout()
+
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
 
 
+#%% Rational Quadratic kernel
+from sklearn.gaussian_process.kernels import RationalQuadratic
+
+kernel = 1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1, alpha_bounds=(1e-5, 1e15))
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 12))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Rational Quadratic kernel", fontsize=18)
+plt.tight_layout()
+
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
+
+#%% Exp-Sine-Squared kernel
+from sklearn.gaussian_process.kernels import ExpSineSquared
+
+kernel = 1.0 * ExpSineSquared(
+    length_scale=1.0,
+    periodicity=3.0,
+    length_scale_bounds=(0.1, 10.0),
+    periodicity_bounds=(1.0, 10.0),
+)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Exp-Sine-Squared kernel", fontsize=18)
+plt.tight_layout()
+
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
 
 
+#%% Dot-product kernel
+from sklearn.gaussian_process.kernels import ConstantKernel, DotProduct
 
+kernel = ConstantKernel(0.1, (0.01, 10.0)) * (
+    DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2
+)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0, normalize_y=True)
 
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
 
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
 
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
 
+fig.suptitle("Dot-product kernel", fontsize=18)
+plt.tight_layout()
 
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
 
+#%% Matérn kernel
+from sklearn.gaussian_process.kernels import Matern
 
+kernel = 1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0), nu=1.5)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
 
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
 
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
 
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
 
+fig.suptitle("Matérn kernel", fontsize=18)
+plt.tight_layout()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
 
 
 
