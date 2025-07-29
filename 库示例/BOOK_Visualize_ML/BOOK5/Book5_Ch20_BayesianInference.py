bc9a2daeaef130c0458005546f6ba58267130740
sysu-server <2716705056@qq.com>
2025-07-29 22:43:10 +0800

    just update

--- 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch20_BayesianInference.py
+++ 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch20_BayesianInference.py
@@ -22,7 +22,7 @@ mask = (Data_all_trials == 1)
 axs[0].plot(Data_all_trials[mask], trials_array[mask], color = 'b', linestyle = None, marker = '.', markersize = 20, label = 'Rabbit')
 axs[0].plot(Data_all_trials[~mask], trials_array[~mask], color = 'r', linestyle = None, marker = '.', markersize = 20, label = 'Chicken')
 axs[0].plot(Data_all_trials, trials_array, color = [0.8, 0.8, 0.8])
-axs[0].set_ylim(1, 200)
+axs[0].set_ylim(1, K)
 axs[0].set_xlim(-0.5, 1.5)
 axs[0].set_xticks([0, 1])
 
@@ -39,7 +39,7 @@ ratio_chickens = 1 - ratio_rabbits
 
 axs[1].plot(ratio_rabbits, trials_array, color = 'b', label = 'Rabbit')
 axs[1].plot(ratio_chickens, trials_array, color = 'r', label = 'Chicken')
-axs[1].set_ylim(1, 200)
+axs[1].set_ylim(1, K)
 axs[1].set_xlim(0, 1)
 axs[1].set_xticks([0, 0.5, 1])
 axs[1].set_ylabel("Number of trials", rotation = 90)
@@ -59,7 +59,7 @@ plt.close()
 from matplotlib.pyplot import cm
 theta_array = np.linspace(0, 1, 500)
 
-num_animals_array = np.arange(0, K + 5, 20)
+num_animals_array = np.arange(0, K + 5, 50)
 num_animals_array = num_animals_array[::-1]
 # reverse the sequence of layers
 colors = cm.rainbow_r(np.linspace(0, 1, len(num_animals_array)))
@@ -113,6 +113,61 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
 plt.show()
 plt.close()
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 图 15. 五个不同参数 α 取不同值时 Beta(α, α) 分布 PDF 图像
+theta_array = np.linspace(0, 1, 100)
+alpha_list = [0.5, 1, 2, 4, 8, 16]
+fig, axs = plt.subplots(nrows=1, ncols=len(alpha_list), figsize=(len(alpha_list)*4, 3))
+for ax_idx, alpha in zip(axs.ravel(), alpha_list):
+    # random data generator
+    # data_idx = Data_all_trials[0:num_animals_idx]
+    # actual percentage of rabbits is 30%
+    # num_rabbits_idx = data_idx.sum() # s
+    pdf = stats.beta.pdf(theta_array,
+                       alpha,
+                       alpha)
+    ax_idx.plot(theta_array, pdf)
+    ax_idx.set_title(f"alpha = {alpha}")
+
+plt.show()
+plt.close()
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 图 17. 似然分布，n = 20
+import scipy.stats as stats
+
+
+theta_array = np.linspace(0, 1, 100)
+n = 20
+slist = np.arange(1, 20, 2)
+colors = cm.rainbow(np.linspace(0, 1, len(slist)))
+
+fig, ax = plt.subplots(1, 2, figsize = (12, 6))
+for i, s in enumerate(slist):
+    ax[0].plot(theta_array, stats.binom.pmf(s, n, theta_array), label=f"{s}", c=colors[i])  # Book06_chap20_(29)
+    # ax[1].plot(theta_array, theta_array**s * (1-theta_array)**(n-s), label=f"{s}", c=colors[i])  # Book06_chap20_(29)
+plt.legend(ncols = 2)
+plt.show()
+plt.close()
+
+# 图 18. 似然分布和 MLE 优化解的位置，n = 20
+theta_array = np.linspace(0, 1, 100)
+n = 20
+slist = np.arange(0, 21, 1)
+colors = cm.rainbow(np.linspace(0, 1, len(slist)))
+
+fig, ax = plt.subplots(1, 1, figsize = (8, 6))
+for i, s in enumerate(slist):
+    ax.axvline(x = s/n, c=colors[i], linestyle='--',)
+    ax.plot(theta_array, (n+1)*stats.binom.pmf(s, n, theta_array) , label=f"{s}", c=colors[i])  # Book06_chap20_(29)
+plt.legend(ncols = 2)
+plt.show()
+plt.close()
+
+# binom.pmf(x, K, p)
+
+
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bk5_Ch20_02
 # 图 25. 对比先验分布、似然分布、后验分布，α 取不同值时
 import scipy.stats as stats
@@ -130,24 +185,21 @@ binom_dist = stats.binom(n, p)
 mu = binom_dist.mean()
 theta_array = np.linspace(0, 1, 500)
 
-# prior distribution
-# assumption: 1:1 ratio
-# i.e., alpha = beta in Beta(alpha, beta) distribution
+# prior distribution  assumption: 1:1 ratio, i.e., alpha = beta in Beta(alpha, beta) distribution
 alpha_arrays = [1, 2, 8, 16, 32, 64]
 for alpha in alpha_arrays:
     beta = alpha
-    prior = stats.beta(alpha, beta)
+    prior = stats.beta(alpha, beta)  # Book06_chap20_(23)
     # posterior distribution
-    posterior = stats.beta(s + alpha, n - s + beta)
+    posterior = stats.beta(s + alpha, n - s + beta)  # Book06_chap20_(34)
     fig, ax = plt.subplots(figsize = (12,6))
     ax.plot(theta_array, prior.pdf(theta_array), label='Prior', c='b')
-    ax.plot(theta_array, posterior.pdf(theta_array), label='Posterior', c='r')
+    ax.plot(theta_array, posterior.pdf(theta_array), label='Posterior', c='r') # 最大化后验概率 MAP
 
     # factor_normalize = stats.binom(n, theta_array).pmf(s).sum()*1/500
     factor_normalize = 1/(n + 1)
-    # note: multiplication factor normalize to
-    # normalize likelihood distribution
-    ax.plot(theta_array, stats.binom(n, theta_array).pmf(s)/factor_normalize, label='Likelihood', c='g')
+    # note: multiplication factor normalize to normalize likelihood distribution
+    ax.plot(theta_array, stats.binom(n, theta_array).pmf(s)/factor_normalize, label='Likelihood', c='g')  # Book06_chap20_(29)
 
     # Prior mode
     try:
@@ -171,7 +223,7 @@ for alpha in alpha_arrays:
 
 #%% 3D visualizations Prior distributions
 # 图 26. 先验分布，α 取不同值时
-a_list = np.arange(1,64 + 1)
+a_list = np.arange(1, 64 + 1)
 theta_MAP = (s + a_list - 1)/(n + 2*a_list - 2)
 theta_MLE = s/n
 
@@ -186,9 +238,9 @@ for a_idx in a_list:
     Prior_PDF_matrix.append(pdf_idx)
 
 Prior_PDF_matrix = np.array(Prior_PDF_matrix)
-
+tt, aa = np.meshgrid(theta_array, a_list)
 fig, ax = plt.subplots(figsize=(10,10))
-ax.contourf(theta_array, a_list, Prior_PDF_matrix, levels = np.linspace(0,Prior_PDF_matrix.max()*1.2,10), cmap = 'Blues')
+ax.contourf(tt, aa, Prior_PDF_matrix, levels = np.linspace(0, Prior_PDF_matrix.max()*1.2, 10), cmap = 'Blues')
 
 # plt.plot(theta_MAP,a, color = 'k')
 # plt.axvline(x = theta_MLE, color = 'k')
@@ -204,11 +256,9 @@ plt.show()
 plt.close()
 
 fig, ax = plt.subplots(figsize=(10,10),subplot_kw={'projection': '3d'})
-tt, aa = np.meshgrid(theta_array,a_list)
+tt, aa = np.meshgrid(theta_array, a_list)
 ax.plot_wireframe(tt, aa, Prior_PDF_matrix, color = [0,0,0], linewidth = 0.25, rstride=3, cstride=0)
-
 ax.contour(theta_array,a_list, Prior_PDF_matrix, levels = np.linspace(0,Prior_PDF_matrix.max()*1.2,10), cmap = 'Blues')
-
 ax.set_proj_type('ortho')
 
 ax.set_xlabel('Theta')
@@ -233,12 +283,10 @@ for a_idx in a_list:
     posterior = stats.beta(s + a_idx, n - s + a_idx)
     pdf_idx = posterior.pdf(theta_array)
     Prior_PDF_matrix.append(pdf_idx)
-
 Prior_PDF_matrix = np.array(Prior_PDF_matrix)
 
 fig, ax = plt.subplots(figsize=(10,10))
 ax.contourf(theta_array, a_list, Prior_PDF_matrix, levels = np.linspace(0,Prior_PDF_matrix.max()*1.2,10), cmap = 'Blues')
-
 ax.plot(theta_MAP,a_list, color = 'k')
 ax.axvline(x = theta_MLE, color = 'k')
 # prior mode
@@ -262,7 +310,6 @@ ax.set_ylabel('Alpha')
 ax.set_xlim(0,1)
 ax.set_ylim(1,a_list.max())
 ax.set_yticks([1,2,8, 16, 32, 64])
-
 ax.set_zlim3d([0,30])
 ax.view_init(azim=-120, elev=30)
 plt.tight_layout()
--- 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch21_DiveintoBayesianInference.py
+++ 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch21_DiveintoBayesianInference.py
@@ -12,22 +12,19 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import scipy.stats as stats
 
-beta_dist = stats.beta
 
 #%% Generate random data
-
 animal_choices = ['rabbit', 'chicken', 'piglet']
 num_animals = 200
 true_prob   = [0.3, 0.6, 0.1]
-animal_array = np.random.choice(animal_choices, num_animals, p=true_prob)
-animal_array = np.insert(animal_array,0,0)
+animal_array = np.random.choice(animal_choices, num_animals, p = true_prob)
+animal_array = np.insert(animal_array, 0, 0)
 
-animal_df = pd.DataFrame(data = animal_array, columns = {'Animal'})
+animal_df = pd.DataFrame(data=animal_array, columns=['Animal'])
 
 animal_df['x1_chicken'] = 0
 animal_df['x2_rabbit']  = 0
 animal_df['x3_piglet']  = 0
-
 
 animal_df.loc[animal_df['Animal'] == 'chicken', 'x1_chicken'] = 1
 animal_df.loc[animal_df['Animal'] == 'rabbit',  'x2_rabbit']  = 1
@@ -50,16 +47,13 @@ animal_df['x3_ratio'] = animal_df['x3_cumsum']/animal_df['total']
 
 animal_df[['x1_ratio','x2_ratio','x3_ratio']] = animal_df[['x1_ratio','x2_ratio','x3_ratio']].bfill()
 
-#%% visualize data of trials
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(18, 30))
-trials_array = np.arange(0,201)+1
+#%% 图 4. 某次试验的蒙特卡罗模拟结果
+fig, axs = plt.subplots(nrows = 1, ncols = 3, figsize = (18, 30))
+trials_array = np.arange(0, 201)+1
 
 axs[0].plot(animal_df['x1_plot'], trials_array, color = 'r', linestyle = None, marker = '.', markersize = 20, label = 'Chicken')
-
 axs[0].plot(animal_df['x2_plot'], trials_array, color = 'b', linestyle = None, marker = '.', markersize = 20, label = 'Rabbit')
-
 axs[0].plot(animal_df['x3_plot'], trials_array, color = 'g', linestyle = None, marker = '.', markersize = 20, label = 'Piglet')
-
 axs[0].plot(animal_df['total_plot'], trials_array, color = [0.8,0.8,0.8])
 
 axs[0].set_ylim(1,200)
@@ -74,16 +68,12 @@ axs[0].set_xlabel("Result of each trial")
 axs[0].legend()
 
 axs[1].plot(animal_df['x1_ratio'],trials_array, color = 'r', label = 'Chicken')
-
 axs[1].plot(animal_df['x2_ratio'], trials_array, color = 'b', label = 'Rabbit')
-
 axs[1].plot(animal_df['x3_ratio'], trials_array, color = 'g', label = 'Piglet')
-
-axs[1].set_ylim(1,200)
-axs[1].set_xlim(0,1)
-axs[1].set_xticks([0,0.5,1])
-
-axs[1].set_ylabel("Number of trials", rotation=90)
+axs[1].set_ylim(1, 200)
+axs[1].set_xlim(0, 1)
+axs[1].set_xticks([0, 0.5, 1])
+axs[1].set_ylabel("Number of trials", rotation = 90)
 # axs[1].yaxis.tick_right()
 # axs[1].yaxis.set_label_position("right")
 axs[1].set_xlabel("Ratio")
@@ -91,12 +81,15 @@ axs[1].set_xlabel("Ratio")
 axs[1].legend()
 
 animal_df[['x2_ratio', 'x1_ratio', 'x3_ratio']].plot.barh(stacked=True, ax = axs[2])
-
 axs[2].set_ylim(1,200)
 axs[2].set_xlim(0,1)
 axs[2].set_xticks([0,0.5,1])
 
-#%% set up alphas
+plt.show()
+plt.close()
+
+
+#%% 图 15. 某次试验的后验边缘分布山脊图，先验分布为 Dir(8, 8, 8)
 
 alpha_1 = 8 # 1, 2
 alpha_2 = 8 # 1, 2
@@ -104,26 +97,23 @@ alpha_3 = 8 # 1, 2
 
 alpha_0 = alpha_1 + alpha_2 + alpha_3
 
-#%% Continuous variations of the posterior
-# ridgeline style
+#%% Continuous variations of the posterior ridgeline style
 
 from matplotlib.pyplot import cm
 theta_array = np.linspace(0, 1, 500)
 
-num_animals_array = np.arange(0,200 + 5,5)
+num_animals_array = np.arange(0, 200 + 5, 5)
 num_animals_array = num_animals_array[::-1]
 # reverse the sequence of layers
 
 colors = cm.rainbow_r(np.linspace(0, 1, len(num_animals_array)))
 theta_array = np.linspace(0, 1, 500)
 
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(18, 30))
-alphas = [alpha_1,alpha_2,alpha_3]
+fig, axs = plt.subplots(nrows = 1, ncols = 3, figsize = (18, 30))
+alphas = [alpha_1, alpha_2, alpha_3]
 x_3_choices = ['x1_cumsum','x2_cumsum','x3_cumsum']
-for ax_idx,alpha_prior_idx,x_idx in zip(axs.ravel(),alphas,x_3_choices):
-
+for ax_idx, alpha_prior_idx, x_idx in zip(axs.ravel(), alphas, x_3_choices):
     for idx, num_animals_idx in enumerate(num_animals_array):
-
         height = num_animals_idx
         # random data generator
         num_chickens_idx = animal_df.iloc[num_animals_idx][x_idx]
@@ -131,22 +121,24 @@ for ax_idx,alpha_prior_idx,x_idx in zip(axs.ravel(),alphas,x_3_choices):
         # Selected animal
         alpha_idx = alpha_prior_idx + num_chickens_idx
         beta_idx  = alpha_0 + num_animals_idx - alpha_idx
-        posterior_pdf = beta_dist.pdf( theta_array, alpha_idx, beta_idx)
+        posterior_pdf = stats.beta.pdf( theta_array, alpha_idx, beta_idx)
 
         loc_max_1 = theta_array[np.argmax(posterior_pdf)]
         # location of MAP
 
         ratio = 1.2
         ax_idx.plot(theta_array, posterior_pdf * ratio + height, color = [0.6,0.6,0.6])
-
         ax_idx.fill_between(theta_array, height, posterior_pdf * ratio + height, color=colors[idx])
 
     ax_idx.set_xlim(0,1)
     ax_idx.set_xlabel('Posterior')
     ax_idx.set_ylabel('Number of trials')
+plt.show()
+plt.close()
 
-#%% snapshots of posterior curves
-# locations of snapshots
+
+#%% snapshots of posterior curves locations of snapshots,
+# 图 16. 九张不同节点的后验边缘 PDF 曲线快照，先验分布为 Dir(8, 8, 8)
 num_animals_array = [0, 1, 2, 3, 4, 5, 10, 100, 200]
 fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
 for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
@@ -158,7 +150,7 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     # Chickens:
     alpha_1_idx = alpha_1 + num_chickens_idx
     beta_1_idx  = alpha_0 + num_animals_idx - alpha_1_idx
-    posterior_pdf_1 = beta_dist.pdf( theta_array, alpha_1_idx, beta_1_idx)
+    posterior_pdf_1 = stats.beta.pdf( theta_array, alpha_1_idx, beta_1_idx)
 
     loc_max_1 = theta_array[np.argmax(posterior_pdf_1)]
     # location of MAP, marginal Beta
@@ -169,7 +161,7 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     # rabbits
     alpha_2_idx = alpha_2 + num_rabbits_idx
     beta_2_idx  = alpha_0 + num_animals_idx - alpha_2_idx
-    posterior_pdf_2 = beta_dist.pdf( theta_array, alpha_2_idx, beta_2_idx)
+    posterior_pdf_2 = stats.beta.pdf( theta_array, alpha_2_idx, beta_2_idx)
 
     loc_max_2 = theta_array[np.argmax(posterior_pdf_2)]
     # location of MAP, marginal Beta
@@ -180,7 +172,7 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     # piglets
     alpha_3_idx = alpha_3 + num_piglets_idx
     beta_3_idx  = alpha_0 + num_animals_idx - alpha_3_idx
-    posterior_pdf_3 = beta_dist.pdf( theta_array, alpha_3_idx, beta_3_idx)
+    posterior_pdf_3 = stats.beta.pdf( theta_array, alpha_3_idx, beta_3_idx)
 
     loc_max_3 = theta_array[np.argmax(posterior_pdf_3)]
     # location of MAP, marginal Beta
@@ -194,8 +186,12 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     ax_idx.set_ylim(0,15)
 
     ax_idx.set_title("animals: %d; chickens: %d; rabbits: %d; piglets: %d;" % (num_animals_idx, num_chickens_idx, num_rabbits_idx, num_piglets_idx))
+plt.show()
+plt.close()
+
 
 #%% snapshots of Dirichlet distribution
+# 图 14. 九张 Dirichlet 分布，θ1θ2平面直角坐标系，先验分布为 Dir(8, 8, 8)
 x1 = np.linspace(0,1,201)
 x2 = np.linspace(0,1,201)
 
@@ -236,4 +232,16 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     ax_idx.set_ylabel(r'$\theta_2$')
     ax_idx.set_box_aspect(1)
     ax_idx.set_title(title_txt)
+plt.show()
+plt.close()
+
+
+
+
+
+
+
+
+
+
 
