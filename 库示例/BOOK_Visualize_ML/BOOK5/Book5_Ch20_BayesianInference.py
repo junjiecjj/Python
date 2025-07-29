6b437f2fb8383b8df27f80291434e13cdbdae156
sysu-server <2716705056@qq.com>
2025-07-29 11:41:02 +0800

    just update

--- 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch20_BayesianInference.py
+++ 库示例/BOOK_Visualize_ML/BOOK5/Book5_Ch20_BayesianInference.py
@@ -5,56 +5,44 @@
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bk5_Ch20_01
 import numpy as np
 from matplotlib import pyplot as plt
-
 import scipy.stats as stats
 
-beta_dist = stats.beta
-
 alpha = 1 # degree of belief # 1, 2, 16
-
 true_percentage = 0.45 # 0.3
-Data_all_trials = stats.bernoulli.rvs(true_percentage, size=200)
-
+K = 2000
+Data_all_trials = stats.bernoulli.rvs(true_percentage, size = K)
 
 #%% visualize data of trials
+# 图 8. 某次试验的模拟结果，先验分布为 Beta(1, 1)
+fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize = (15, 30))
 
-fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15, 30))
-
-trials_array = np.arange(0,200)+1
+trials_array = np.arange(0, K) + 1
 mask = (Data_all_trials == 1)
 
-axs[0].plot(Data_all_trials[mask],trials_array[mask], color = 'b', linestyle = None, marker = '.', markersize = 20, label = 'Rabbit')
+axs[0].plot(Data_all_trials[mask], trials_array[mask], color = 'b', linestyle = None, marker = '.', markersize = 20, label = 'Rabbit')
+axs[0].plot(Data_all_trials[~mask], trials_array[~mask], color = 'r', linestyle = None, marker = '.', markersize = 20, label = 'Chicken')
+axs[0].plot(Data_all_trials, trials_array, color = [0.8, 0.8, 0.8])
+axs[0].set_ylim(1, 200)
+axs[0].set_xlim(-0.5, 1.5)
+axs[0].set_xticks([0, 1])
 
-axs[0].plot(Data_all_trials[~mask],trials_array[~mask], color = 'r', linestyle = None, marker = '.', markersize = 20, label = 'Chicken')
-
-axs[0].plot(Data_all_trials,trials_array, color = [0.8,0.8,0.8])
-
-axs[0].set_ylim(1,200)
-axs[0].set_xlim(-0.5,1.5)
-axs[0].set_xticks([0,1])
-
-axs[0].set_ylabel("Number of trials", rotation=90)
+axs[0].set_ylabel("Number of trials", rotation = 90)
 # axs[0].yaxis.tick_right()
 # axs[0].yaxis.set_label_position("right")
 axs[0].set_xlabel("Result of each trial")
 # axs[0].invert_xaxis()
 axs[0].legend()
-
 # plt.setp(axs[0].get_xticklabels(), rotation=90, va="top", ha="center")
 # plt.setp(axs[0].get_yticklabels(), rotation=90, va="center", ha="left")
-
 ratio_rabbits = np.cumsum(Data_all_trials)/trials_array
 ratio_chickens = 1 - ratio_rabbits
 
-axs[1].plot(ratio_rabbits,trials_array, color = 'b', label = 'Rabbit')
-
-axs[1].plot(ratio_chickens,trials_array, color = 'r', label = 'Chicken')
-
-axs[1].set_ylim(1,200)
-axs[1].set_xlim(0,1)
-axs[1].set_xticks([0,0.5,1])
-
-axs[1].set_ylabel("Number of trials", rotation=90)
+axs[1].plot(ratio_rabbits, trials_array, color = 'b', label = 'Rabbit')
+axs[1].plot(ratio_chickens, trials_array, color = 'r', label = 'Chicken')
+axs[1].set_ylim(1, 200)
+axs[1].set_xlim(0, 1)
+axs[1].set_xticks([0, 0.5, 1])
+axs[1].set_ylabel("Number of trials", rotation = 90)
 # axs[1].yaxis.tick_right()
 # axs[1].yaxis.set_label_position("right")
 axs[1].set_xlabel("Ratio")
@@ -63,22 +51,20 @@ axs[1].legend()
 
 # plt.setp(axs[1].get_xticklabels(), rotation=90, va="top", ha="center")
 # plt.setp(axs[1].get_yticklabels(), rotation=90, va="center", ha="left")
+plt.show()
+plt.close()
 
-
-#%% Continuous variations of the posterior
-# ridgeline style
-
+#%% Continuous variations of the posterior ridgeline style
+# 图 8. 某次试验的模拟结果，先验分布为 Beta(1, 1)
 from matplotlib.pyplot import cm
 theta_array = np.linspace(0, 1, 500)
 
-num_animals_array = np.arange(0,200 + 5,5)
+num_animals_array = np.arange(0, K + 5, 20)
 num_animals_array = num_animals_array[::-1]
 # reverse the sequence of layers
-
 colors = cm.rainbow_r(np.linspace(0, 1, len(num_animals_array)))
 
-fig, ax = plt.subplots(figsize=(8, 30))
-
+fig, ax = plt.subplots(figsize=(8, 60))
 for idx, num_animals_idx in enumerate(num_animals_array):
     height = num_animals_idx
     # random data generator
@@ -86,38 +72,31 @@ for idx, num_animals_idx in enumerate(num_animals_array):
     # actual percentage of rabbits is 30%
 
     num_rabbits_idx = data_idx.sum() # s
-    posterior_pdf = beta_dist.pdf(theta_array,
+    posterior_pdf = stats.beta.pdf(theta_array,
                       num_rabbits_idx + alpha,  # s + alpha
                       num_animals_idx - num_rabbits_idx + alpha) # n - s + alpha
-
     ratio = 1.2
     ax.plot(theta_array, posterior_pdf * ratio + height, color = [0.6,0.6,0.6])
-
     ax.fill_between(theta_array, height, posterior_pdf * ratio + height, color=colors[idx])
 
 ax.set_xlim(0,1)
 ax.set_xlabel('Posterior')
 ax.set_ylabel('Number of trials')
+plt.show()
+plt.close()
 
-
-#%% snapshots of posterior curves
-# locations of snapshots
-
+#%% snapshots of posterior curves locations of snapshots
+# 图 9. 九张不同节点的后验概率分布曲线快照，先验分布为 Beta(1, 1)
 num_animals_array = [0, 1, 2, 3, 4, 5, 10, 100, 200]
-
 fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
-
 for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
-
     # random data generator
     data_idx = Data_all_trials[0:num_animals_idx]
     # actual percentage of rabbits is 30%
-
     num_rabbits_idx = data_idx.sum() # s
-    posterior_pdf = beta_dist.pdf(theta_array,
+    posterior_pdf = stats.beta.pdf(theta_array,
                       num_rabbits_idx + alpha,  # s + alpha
                       num_animals_idx - num_rabbits_idx + alpha) # n - s + alpha
-
     loc_max = theta_array[np.argmax(posterior_pdf)]
     # location of MAP
 
@@ -131,16 +110,17 @@ for ax_idx, num_animals_idx in zip(axs.ravel(), num_animals_array):
     ax_idx.set_xlim(0,1)
     ax_idx.set_yticks([0,5,10,15])
     ax_idx.set_ylim(0,15)
+plt.show()
+plt.close()
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bk5_Ch20_02
-
+# 图 25. 对比先验分布、似然分布、后验分布，α 取不同值时
 import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
 
 # number of animal samples:
 n = 200
-
 # number of rabbits in data:
 s = 60
 
@@ -153,9 +133,7 @@ theta_array = np.linspace(0, 1, 500)
 # prior distribution
 # assumption: 1:1 ratio
 # i.e., alpha = beta in Beta(alpha, beta) distribution
-
 alpha_arrays = [1, 2, 8, 16, 32, 64]
-
 for alpha in alpha_arrays:
     beta = alpha
     prior = stats.beta(alpha, beta)
@@ -176,10 +154,8 @@ for alpha in alpha_arrays:
         ax.axvline((alpha-1)/(alpha+beta-2), c='b', linestyle='--', label='Prior mode')
     except:
         pass
-
     # MAP
     ax.axvline((s+alpha-1)/(n+alpha+beta-2), c='r', linestyle='--', label='MAP')
-
     # MLE
     ax.axvline(mu/n, c='g', linestyle='--', label='MLE')
 
@@ -191,12 +167,11 @@ for alpha in alpha_arrays:
     ax.legend()
     plt.show()
     plt.close()
-#%% 3D visualizations
 
-#%% Prior distributions
 
+#%% 3D visualizations Prior distributions
+# 图 26. 先验分布，α 取不同值时
 a_list = np.arange(1,64 + 1)
-
 theta_MAP = (s + a_list - 1)/(n + 2*a_list - 2)
 theta_MLE = s/n
 
@@ -211,7 +186,6 @@ for a_idx in a_list:
     Prior_PDF_matrix.append(pdf_idx)
 
 Prior_PDF_matrix = np.array(Prior_PDF_matrix)
-
 
 fig, ax = plt.subplots(figsize=(10,10))
 ax.contourf(theta_array, a_list, Prior_PDF_matrix, levels = np.linspace(0,Prior_PDF_matrix.max()*1.2,10), cmap = 'Blues')
@@ -250,10 +224,8 @@ ax.grid(False)
 plt.show()
 plt.close()
 
-#%% Posterior distribution
-
+#%% Posterior distribution 图 27. 后验分布，α 取不同值时
 Prior_PDF_matrix = []
-
 for a_idx in a_list:
     print(s + a_idx)
     print(n - s + a_idx)
@@ -263,7 +235,6 @@ for a_idx in a_list:
     Prior_PDF_matrix.append(pdf_idx)
 
 Prior_PDF_matrix = np.array(Prior_PDF_matrix)
-
 
 fig, ax = plt.subplots(figsize=(10,10))
 ax.contourf(theta_array, a_list, Prior_PDF_matrix, levels = np.linspace(0,Prior_PDF_matrix.max()*1.2,10), cmap = 'Blues')
@@ -280,7 +251,6 @@ ax.set_ylim(1,a_list.max())
 ax.set_yticks([1,2,8, 16, 32, 64])
 plt.show()
 plt.close()
-
 
 fig, ax = plt.subplots(figsize=(10,10), subplot_kw={'projection': '3d'})
 tt, aa = np.meshgrid(theta_array,a_list)
