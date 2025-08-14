9f7b0cf5adc5f9d416c9921164c52411534ea7d4
sysu-server <2716705056@qq.com>
2025-08-14 18:04:27 +0800

    just update

--- 通信仿真/无线通信系统/DigitalModulationsPython/Chap14_圆卷积.py
+++ 通信仿真/无线通信系统/DigitalModulationsPython/Chap14_圆卷积.py
@@ -9,8 +9,25 @@ import scipy
 import numpy as np
 # import statsmodels.tsa.api as smt
 import matplotlib.pyplot as plt
-# from matplotlib.font_manager import FontProperties
-# import commpy
+# 全局设置字体大小
+# plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.family"] = "SimSun"
+plt.rcParams['font.size'] = 14  # 设置全局字体大小
+plt.rcParams['axes.titlesize'] = 16  # 设置坐标轴标题字体大小
+plt.rcParams['axes.labelsize'] = 16  # 设置坐标轴标签字体大小
+plt.rcParams['xtick.labelsize'] = 12  # 设置 x 轴刻度字体大小
+plt.rcParams['ytick.labelsize'] = 12  # 设置 y 轴刻度字体大小
+plt.rcParams['axes.unicode_minus'] = False # 用来显示负号
+plt.rcParams["figure.figsize"] = [8, 6] # 调整生成的图表最大尺寸
+# plt.rcParams['figure.dpi'] = 300 # 每英寸点数
+plt.rcParams['lines.linestyle'] = '--'
+plt.rcParams['lines.linewidth'] = 2     # 线条宽度
+plt.rcParams['lines.color'] = 'blue'
+plt.rcParams['lines.markersize'] = 6 # 标记大小
+# plt.rcParams['figure.facecolor'] = 'lightgrey'  # 设置图形背景色为浅灰色
+plt.rcParams['figure.facecolor'] = 'white'  # 设置图形背景色为浅灰色
+plt.rcParams['axes.edgecolor'] = 'black'  # 设置坐标轴边框颜色为黑色
+plt.rcParams['legend.fontsize'] = 12
 
 #%%
 # 生成 循环矩阵
@@ -50,6 +67,7 @@ s = np.array([-0.0155, 2.5770, 1.9238, -0.0629, -0.8105, 0.6727, -1.5924, -0.800
 
 lin_s_h = scipy.signal.convolve(h, s)
 cir_s_h = circularConvolve(h, s, N)
+print(f"lin_s_h = \n{lin_s_h}\ncir_s_h = \n{cir_s_h}")
 
 Ncp = 2
 s_cp = np.hstack((s[-Ncp:], s))
