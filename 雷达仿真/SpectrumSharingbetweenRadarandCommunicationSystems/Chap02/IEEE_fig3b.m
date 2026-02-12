b42a59ad7cc6d64cae6c03a67f4432b98134c0af
sysu-server <2716705056@qq.com>
2026-02-12 12:28:06 +0800

    just update

--- 雷达仿真/SpectrumSharingbetweenRadarandCommunicationSystems/Chap02/IEEE_fig3b.m
+++ 雷达仿真/SpectrumSharingbetweenRadarandCommunicationSystems/Chap02/IEEE_fig3b.m
@@ -34,55 +34,61 @@ SNR_db = -8:1:10;
 SNR_mag = 10.^(SNR_db./10); 
 %Probability of false alarm values 
 P_FA = [10^-5];
-P_FA = [10^-1, 10^-2];
+P_FA = [10^-1, 10^-5];
 %% Monte-Carlo iterations 
 MC_iter = 11; 
+BS = 5;
 
-for i=1:MC_iter %% Interference channel matrix H generation and null space computation
-    % Generate Cellular Channels and Find NS of them and select 
-    % Channel with Min NS 
-    BS = 5; 
-    % Make a cell to store matrices 
-    BS_channels = cell(1,BS); 
-    % Make a cell to store Projectors for every BS
-    Proj_matrix = cell(1,BS);
-    Pd_orthog_cell = cell(1, BS); 
-    Pd_NSP_cell    = cell(1, BS); 
+Pd_orthog_it = zeros(MC_iter, BS, length(SNR_mag), length(P_FA));
+Pd_NSP_it = zeros(MC_iter, BS, length(SNR_mag), length(P_FA));
+for i=1:MC_iter
+    Pd_orthog_bs = zeros(BS, length(SNR_mag), length(P_FA));
+    Pd_NSP_bs = zeros(BS, length(SNR_mag), length(P_FA));
     for b = 1:BS
-        BS_channels{b} = (randn(Nr,Mt)+1i*randn(Nr,Mt)); 
-        Proj_matrix{b} = null(BS_channels{b}) * ctranspose(null(BS_channels{b})); 
-        Rs_null{b} = Proj_matrix{b} * Rs * Proj_matrix{b}';
+        BS_channels = (randn(Nr,Mt)+1i*randn(Nr,Mt)); 
+        Proj_matrix = null(BS_channels) * ctranspose(null(BS_channels)); 
+        Rs_null     = Proj_matrix * Rs * Proj_matrix';
+        Pd_orthog = zeros(length(SNR_mag), length(P_FA));
+        Pd_NSP    = zeros(length(SNR_mag), length(P_FA));
         % Non-centrality parameter of chi-square
         for z = 1:length(SNR_mag)
-            rho_orthog(b) = SNR_mag(z)*(abs(a'*Rs.'*a))^2;
-            rho_NSP(b) = SNR_mag(z)*(abs(a'*Rs_null{b}.'*a))^2;
+            rho_orthog = SNR_mag(z)*(abs(a'*Rs.'*a))^2;
+            rho_NSP    = SNR_mag(z)*(abs(a'*Rs_null.'*a))^2;
             % Creates threshold values for a desired Pfa for an inverse central-chi-square w/2 degrees of freedom
             delta = chi2inv(ones(1, length(P_FA)) - P_FA, repmat(2, 1, length(P_FA)));
             % rows = SNR, cols = P_FA, ncx2cdf = Noncentral chi -square cumulative distribution function
-            Pd_orthog(z,:) = ones(1, length(P_FA)) - ncx2cdf(delta, repmat(2, 1, length(P_FA)), repmat(rho_orthog(b), 1, length(P_FA )));
-            Pd_NSP(z,:) = ones(1, length(P_FA)) - ncx2cdf(delta,repmat(2, 1, length(P_FA)), repmat(rho_NSP(b), 1, length(P_FA)));
+            Pd_orthog(z,:) = ones(1, length(P_FA)) - ncx2cdf(delta, repmat(2, 1, length(P_FA)), repmat(rho_orthog, 1, length(P_FA )));
+            Pd_NSP(z,:) = ones(1, length(P_FA)) - ncx2cdf(delta,repmat(2, 1, length(P_FA)), repmat(rho_NSP, 1, length(P_FA)));
         end
-        Pd_orthog_cell{b}= Pd_orthog;
-        Pd_NSP_cell{b}= Pd_NSP;
-    end 
-    Pd_orthog_cat(:,:,i) = cell2mat(Pd_orthog_cell);
-    Pd_NSP_cat(:,:,i) = cell2mat(Pd_NSP_cell);
+        Pd_orthog_bs(b,:,:) = Pd_orthog;
+        Pd_NSP_bs(b,:,:) = Pd_NSP;
+    end
+    Pd_orthog_it(i,:,:,:) = Pd_orthog_bs;
+    Pd_NSP_it(i,:,:,:) = Pd_NSP_bs;
 end
-Pd_orthog_cat_mean = mean(Pd_orthog_cat ,3);
-Pd_NSP_cat_mean = mean(Pd_NSP_cat ,3);
-%% Plots Probability of detection curves for given  Probability of false alarm 
-figure(1);
-plot(SNR_db',Pd_NSP_cat_mean(:,1),'g','LineWidth',2.5); hold on;
-plot(SNR_db',Pd_NSP_cat_mean(:,2),'b','LineWidth',2.5); hold on;
-plot(SNR_db',Pd_NSP_cat_mean(:,3),'r','LineWidth',2.5); hold on;
-plot(SNR_db',Pd_NSP_cat_mean(:,4),'m','LineWidth',2.5); hold on;
-plot(SNR_db',Pd_NSP_cat_mean(:,5),'y','LineWidth',2.5); hold on;
-plot(SNR_db',Pd_orthog_cat_mean(:,1),'k','LineWidth',2.5); hold on;
-xlabel('SNR','fontsize' ,14);
-ylabel('P_D','fontsize' ,14);
-title('P_D for P_{FA} = 10^{-5}','fontsize' ,14);
-legend('P_D for NSP Waveforms to BS 1', 'P_D for NSP Waveforms to BS 2', 'P_D for NSP Waveforms to BS 3', 'P_D for NSP Waveforms to BS 4', 'P_D for NSP Waveforms to BS 5', 'P_D for Orthogonal Waveforms')
 
+Pd_orthog_mean = mean(Pd_orthog_it ,1);
+Pd_NSP_mean = mean(Pd_NSP_it ,1);
+
+
+%% Plots Probability of detection curves for given  Probability of false alarm
+colors = ['g', 'b', 'r', 'm', 'y' ];
+for z = 1:length(P_FA)
+    figure(z);
+    for b = 1:BS
+        plot(SNR_db',squeeze(Pd_NSP_mean(1, b,:,z)),colors(b),'LineWidth',2.5); hold on;
+        % plot(SNR_db',Pd_NSP_cat_mean(b,:,z),'b','LineWidth',2.5); hold on;
+        % plot(SNR_db',Pd_NSP_cat_mean(b,:,z),'r','LineWidth',2.5); hold on;
+        % plot(SNR_db',Pd_NSP_cat_mean(b,:,z),'m','LineWidth',2.5); hold on;
+        % plot(SNR_db',Pd_NSP_cat_mean(b,:,z),'y','LineWidth',2.5); hold on;
+    end
+    plot(SNR_db',squeeze(Pd_orthog_mean(1, 1,:,z)),'k','LineWidth',2.5); hold on;
+    xlabel('SNR','fontsize' ,14);
+    ylabel('P_D','fontsize' ,14);
+    str = 1;
+    title('P_D for P_{FA} = 10^{-5}','fontsize' ,14);
+    legend('P_D for NSP Waveforms to BS 1', 'P_D for NSP Waveforms to BS 2', 'P_D for NSP Waveforms to BS 3', 'P_D for NSP Waveforms to BS 4', 'P_D for NSP Waveforms to BS 5', 'P_D for Orthogonal Waveforms')
+end
 
 
 
