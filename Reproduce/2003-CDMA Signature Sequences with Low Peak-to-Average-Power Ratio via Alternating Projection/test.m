68b8f69968c2f975563df8ee1f49be4081d5adca
sysu-server <2716705056@qq.com>
2026-03-30 23:29:19 +0800

    just update

--- Reproduce/2003-CDMA Signature Sequences with Low Peak-to-Average-Power Ratio via Alternating Projection/test.m
+++ Reproduce/2003-CDMA Signature Sequences with Low Peak-to-Average-Power Ratio via Alternating Projection/test.m
@@ -1,40 +1,33 @@
+
+
+
+
+clc;
+clear all;
+close all;
+
+rng(42); 
+
 % 测试初始化正交性
-M = 3; N = 6;
+M = 4; N = 40;
 rng(42);  % 固定种子
 
 % 生成随机矩阵，取 SVD 的前 M 列作为正交基
 X = randn(M, N) + 1j*randn(M, N);
-[U, ~, ~] = svd(X, 'econ');
-if N <= M
-    X = U(:,1:N);
-else
-    X = [U, zeros(M, N-M)];
-    for n = M+1:N
-        v = randn(M,1) + 1j*randn(M,1);
-        v = v - X(:,1:n-1) * (X(:,1:n-1)' * v); % 正交化
-        v = v / norm(v);
-        X(:,n) = v;
-    end
-end
+R = X*X'/N;
 
-% 检查正交性
-fprintf('前 M 列内积（应接近 0）：\n');
-for i = 1:M
-    for j = i+1:M
-        fprintf('(%d,%d): %e + %ei\n', i, j, real(X(:,i)'*X(:,j)), imag(X(:,i)'*X(:,j)));
-    end
-end
 
-fprintf('\n后 N-M 列与前 M 列内积（应接近 0）：\n');
-for i = M+1:N
-    for j = 1:M
-        fprintf('(%d,%d): %e + %ei\n', i, j, real(X(:,i)'*X(:,j)), imag(X(:,i)'*X(:,j)));
-    end
-end
 
-fprintf('\n后 N-M 列之间内积（可以非零）：\n');
-for i = M+1:N
-    for j = i+1:N
-        fprintf('(%d,%d): %e + %ei\n', i, j, real(X(:,i)'*X(:,j)), imag(X(:,i)'*X(:,j)));
-    end
-end
+Rsqrt = R^(0.5);
+
+[L, D] = ldl(R);
+R_sr = sqrt(D) * L';
+
+
+
+A = randn(3, 4) + 1i * randn(3, 4);
+
+[U, S, V] = svd(A);
+
+[U1, S1, V1] = svd(A, 'econ');
+
--- Reproduce/Waveform Design for a Dual-Function MIMO RadCom System/functions/getzn.m
+++ Reproduce/Waveform Design for a Dual-Function MIMO RadCom System/functions/getzn.m
@@ -1,9 +1,20 @@
 
 
+
+
+
+% function zn = getzn(Z, M, P, n)
+%     zn = zeros(M, 1);
+%     for p = 1:P
+%         zn = zn + Z(p:p+M-1, (n-1)*P + p);
+%     end
+%     zn = zn / P;
+% end
+
 function zn = getzn(Z, M, P, n)
-    zn = zeros(M, 1);
+    zn = zeros(1, M);
     for p = 1:P
-        zn = zn + Z(p:p+M-1, (n-1)*P + p);
+        zn = zn + Z((n-1)*P + p, p:p+M-1);
     end
     zn = zn / P;
 end
