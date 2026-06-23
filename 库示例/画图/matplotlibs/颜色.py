beb6cb1b5bf5840e7253aff8a8a58dd3054735de
sysu-server <2716705056@qq.com>
2026-06-23 22:43:22 +0800

    just update

---
+++ 库示例/画图/matplotlibs/颜色.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 17:13:49 2026
+
+@author: jack
+"""
+
+
+
+colors = ['#F65314', '#00A1F1', '#77AC30', '#8A2BE2', '#00A8BB', '#694AC8', '#FFB7DB', '#05C349', '#FF8853', '', '', '', ]
