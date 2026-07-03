64e502384815e774581f91ca0c0f717277cdf92a
sysu-server <2716705056@qq.com>
2026-07-03 23:03:27 +0800

    just update

--- Reproduce/2025-JSAC_Communication-Assisted_Sensing_in_6G_Networks/Colored_MIMO_Capicity.py
+++ Reproduce/2025-JSAC_Communication-Assisted_Sensing_in_6G_Networks/Colored_MIMO_Capicity.py
@@ -146,71 +146,129 @@ print(f"Lambda_x, Theoretical = {Lambda_x1}")
 
 
 #%% Colored 噪声
+# Z = np.random.randn(M, L) + 1j * np.random.randn(M, L)
+# Sigma_Z = Z @ Z.conj().T / L  #  + np.diag(np.random.randint(10, size  = M))   #  N x N
+# Lambda_z, U_z = np.linalg.eig(Sigma_Z)
+# Lambda_z = np.abs(Lambda_z)
+# idx = np.argsort(Lambda_z)[::-1]  # 从大到小
+# Lambda_z = Lambda_z[idx]
+# U_z = U_z[:, idx]
+
+# idx = np.argsort(Lambda_h_hat)    # 从小到大
+# Lambda_h_hat = Lambda_h_hat[idx]
+# V_h = V_h[:, idx]
+
+
+# ## Theoretical solution
+# from WaterFilling import water_filling, plot_waterfilling
+
+# Lambda_x1, water_level = water_filling(Lambda_z[M-N:], Lambda_h_hat, PT)
+# print(f"最优功率分配: {Lambda_x1}/{np.sum(Lambda_x1):.4f}")
+
+# plot_waterfilling(Lambda_z[M-N:]/Lambda_h_hat, Lambda_x1, water_level)
+# Sigma_X = V_h @ np.diag(Lambda_x1) @ V_h.conj().T
+# C1 = np.log(np.linalg.det(Hc@Sigma_X@Hc.conj().T + Sigma_Z))
+
+# ## Use CVX
+# Sigma_x = cp.Variable((N, N), hermitian = True)
+
+# constraints = [0 << Sigma_x,
+#                cp.trace(Sigma_x) <= PT,
+#               ]
+# obj = cp.Maximize(cp.log_det(Hc@Sigma_x@Hc.conj().T + Sigma_Z))
+# prob = cp.Problem(obj, constraints)
+# prob.solve()
+
+# if prob.status=='optimal':
+#      print(f"{prob.value}")
+#       # print(f"{Rc.value}")
+
+# Sigma_x = Sigma_x.value
+# C2 = np.log(np.linalg.det(Hc@Sigma_x@Hc.conj().T + Sigma_Z))
+# Lambda_x, U_x = np.linalg.eig(Sigma_x)
+# Lambda_x = np.abs(Lambda_x)
+
+# Psi_c_aligned, reorder = align_eigenvectors(V_h, U_x)
+# Lambda_x = Lambda_x[reorder]
+
+
+# # 好像上面的理论值和CVX仿真的 Lambda_x 结果有出入，
+# print(f"Lambda_x, cvx         = {Lambda_x}")
+# print(f"Lambda_x, Theoretical = {Lambda_x1}")
+# print(f"C1 = {C1}, C2 = {C2}")  # 但是容量是差不多的，因此这里需要确认，到底是python带来的误差还是理论分析有误
+
+#%% Colored 噪声：正确版本
+
 Z = np.random.randn(M, L) + 1j * np.random.randn(M, L)
-Sigma_Z = Z @ Z.conj().T / L  #  + np.diag(np.random.randint(10, size  = M))   #  N x N
-Lambda_z, U_z = np.linalg.eig(Sigma_Z)
-Lambda_z = np.abs(Lambda_z)
-idx = np.argsort(Lambda_z)[::-1]  # 从大到小
-Lambda_z = Lambda_z[idx]
-U_z = U_z[:, idx]
+Sigma_Z = Z @ Z.conj().T / L
+Sigma_Z = (Sigma_Z + Sigma_Z.conj().T) / 2
 
-idx = np.argsort(Lambda_h_hat)    # 从小到大
-Lambda_h_hat = Lambda_h_hat[idx]
-V_h = V_h[:, idx]
+# 等效信道矩阵对应的 Gram 矩阵：
+# A_eff = H^H Sigma_Z^{-1} H
+A_eff = Hc.conj().T @ np.linalg.solve(Sigma_Z, Hc)
+A_eff = (A_eff + A_eff.conj().T) / 2
 
+# Hermitian 矩阵使用 eigh
+Lambda_eff, V_eff = np.linalg.eigh(A_eff)
 
-## Theoretical solution
-from WaterFilling import water_filling, plot_waterfilling
+# 按等效信道增益从大到小排序
+idx = np.argsort(Lambda_eff)[::-1]
+Lambda_eff = np.real(Lambda_eff[idx])
+V_eff = V_eff[:, idx]
+Lambda_eff = np.maximum(Lambda_eff, 0)
 
-Lambda_x1, water_level = water_filling(Lambda_z[M-N:], Lambda_h_hat, PT)
+# 理论注水解
+Lambda_x1, water_level = water_filling(np.ones(N), Lambda_eff, PT)
 print(f"最优功率分配: {Lambda_x1}/{np.sum(Lambda_x1):.4f}")
 
-plot_waterfilling(Lambda_z[M-N:]/Lambda_h_hat, Lambda_x1, water_level)
-Sigma_X = V_h @ np.diag(Lambda_x1) @ V_h.conj().T
-C1 = np.log(np.linalg.det(Hc@Sigma_X@Hc.conj().T + Sigma_Z))
+plot_waterfilling(1 / Lambda_eff, Lambda_x1, water_level)
 
-## Use CVX
-Sigma_x = cp.Variable((N, N), hermitian = True)
+# 理论最优协方差
+Sigma_X = V_eff @ np.diag(Lambda_x1) @ V_eff.conj().T
+Sigma_X = (Sigma_X + Sigma_X.conj().T) / 2
 
-constraints = [0 << Sigma_x,
-               cp.trace(Sigma_x) <= PT,
-              ]
-obj = cp.Maximize(cp.log_det(Hc@Sigma_x@Hc.conj().T + Sigma_Z))
+# 理论容量
+C1_mat = Hc @ Sigma_X @ Hc.conj().T + Sigma_Z
+C1_mat = (C1_mat + C1_mat.conj().T) / 2
+C1 = np.linalg.slogdet(C1_mat)[1]
+
+#%% Use CVX
+
+Sigma_x_var = cp.Variable((N, N), hermitian=True)
+
+constraints = [
+    Sigma_x_var >> 0,
+    cp.trace(Sigma_x_var) <= PT
+]
+
+obj = cp.Maximize(cp.log_det(Hc @ Sigma_x_var @ Hc.conj().T + Sigma_Z))
 prob = cp.Problem(obj, constraints)
 prob.solve()
 
-if prob.status=='optimal':
-     print(f"{prob.value}")
-      # print(f"{Rc.value}")
+if prob.status == 'optimal':
+    print(f"CVX objective = {prob.value}")
 
-Sigma_x = Sigma_x.value
-C2 = np.log(np.linalg.det(Hc@Sigma_x@Hc.conj().T + Sigma_Z))
-Lambda_x, U_x = np.linalg.eig(Sigma_x)
-Lambda_x = np.abs(Lambda_x)
+Sigma_x_cvx = Sigma_x_var.value
+Sigma_x_cvx = (Sigma_x_cvx + Sigma_x_cvx.conj().T) / 2
 
-Psi_c_aligned, reorder = align_eigenvectors(V_h, U_x)
-Lambda_x = Lambda_x[reorder]
+# CVX 容量
+C2_mat = Hc @ Sigma_x_cvx @ Hc.conj().T + Sigma_Z
+C2_mat = (C2_mat + C2_mat.conj().T) / 2
+C2 = np.linalg.slogdet(C2_mat)[1]
 
+# 比较特征值
+Lambda_x_cvx, U_x_cvx = np.linalg.eigh(Sigma_x_cvx)
 
-# 好像上面的理论值和CVX仿真的 Lambda_x 结果有出入，
-print(f"Lambda_x, cvx         = {Lambda_x}")
+idx = np.argsort(Lambda_x_cvx)[::-1]
+Lambda_x_cvx = np.real(Lambda_x_cvx[idx])
+U_x_cvx = U_x_cvx[:, idx]
+Lambda_x_cvx = np.maximum(Lambda_x_cvx, 0)
+
+print(f"Lambda_x, CVX         = {Lambda_x_cvx}")
 print(f"Lambda_x, Theoretical = {Lambda_x1}")
-print(f"C1 = {C1}, C2 = {C2}")  # 但是容量是差不多的，因此这里需要确认，到底是python带来的误差还是理论分析有误
-
-####
-N = 3
-L = 4
-X = np.random.randn(N, L) + 1j * np.random.randn(N, L)
-Sigma_X = X @ X.conj().T
-evalue, evec = np.linalg.eig(Sigma_X)
-
-Sigma_X1 = X.conj().T @ X
-evalue1, evec1 = np.linalg.eig(Sigma_X1)
-
-U, S, VH = np.linalg.svd(X)  #  S**2 == evalue
-
-
-
+print(f"C1 = {C1}, C2 = {C2}")
+print(f"Capacity gap = {abs(C1 - C2)}")
+print(f"Covariance gap = {np.linalg.norm(Sigma_X - Sigma_x_cvx, 'fro')}")
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Radar Capacity maximization %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  MMSE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
